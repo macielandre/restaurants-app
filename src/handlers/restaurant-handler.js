cd6fb2a004488a5288dd22cc4dbd48879a3fbe02
Andre Gouvea Maciel <andre.maciel@mundiale.com.br>
2022-04-07 08:25:18 -0300

    feat: delete validation

--- src/handlers/restaurant-handler.js
+++ src/handlers/restaurant-handler.js
@@ -2,15 +2,16 @@ const RestaurantValidation = require('../validations/restaurant-validation')
 
 const router = require('express').Router()
 
+const defaultError = 'unexpected error'
+
 router.post('/', async (req, res) => {
-    try{
+    try {
         const restaurant = await RestaurantValidation.restaurantInsert(req.body)
 
         res.status(200).json({ message: 'restaurant created in database', restaurant})
     } catch(err) {
-        res.status(err?.status || 500).json({ message: err.message || 'unexpected error' })
+        res.status(err?.status || 500).json({ message: err.message || defaultError })
     }
-    
 })
 
 router.get('/', async (req, res) => {
@@ -19,16 +20,28 @@ router.get('/', async (req, res) => {
 
         res.status(200).json({ message: 'list of restaurants with given filters', restaurants: null })
     } catch(err) {
-        res.status(err.status || 500).json({ message: err.message || 'unexpected error' })
+        res.status(err.status || 500).json({ message: err.message || defaultError })
     }
 })
 
-router.patch('/', async (req, res) => {
-    res.status(200).json({ test: 'working' })
+router.patch('/:id', async (req, res) => {
+    try {
+        const restaurantUpdateData = await RestaurantValidation.restaurantUpdate(req.params)
+
+        res.status(200).json({ message: 'restaurant updated with new information', restaurant: null })
+    } catch(err) {
+        res.status(err.status || 500).json({ message: err.message || defaultError })
+    }
 })
 
-router.delete('/', async (req, res) => {
-    res.status(200).json({ test: 'working' })
+router.delete('/:id', async (req, res) => {
+    try {
+        const id = await RestaurantValidation.restaurantDelete(req.params)
+
+        res.status(200).json({ message: 'restaurant deleted', restaurant: null })
+    } catch(err) {
+        res.status(err.status || 500).json({ message: err.message || defaultError })
+    }
 })
 
 module.exports = router
--- src/validations/restaurant-validation.js
+++ src/validations/restaurant-validation.js
@@ -13,14 +13,35 @@ class RestaurantValidation {
         return yup.validateSchema(schema, restaurant)
     }
 
+    static async restaurantUpdate(restaurantUpdateData) {
+        const schema = yup.object().shape({
+            id: yup.number().min(0).max(9999),
+            name: yup.string().min(1).uppercase(),
+            address: yup.string().min(1).uppercase(),
+            zipcode: yup.string().min(1),
+            status: yup.string().min(1).uppercase().oneOf(['CHANGE'])
+        })
+
+        return yup.validateSchema(schema, restaurantUpdateData)
+    }
+
     static async restaurantList(restaurantKeys) {
         const schema = yup.object().shape({
             id: yup.number().min(0).max(9999),
             name: yup.string().min(1).uppercase()
         })
 
-        return yup.validateSchema(schema, restaurant)
+        return yup.validateSchema(schema, restaurantKeys)
     }
+
+    static async restaurantDelete(idObject) {
+        const schema = yup.object().shape({
+            id: yup.number().min(0).max(9999).required()
+        })
+
+        return yup.validateSchema(schema, idObject)
+    }
+
 }
 
 module.exports = RestaurantValidation
